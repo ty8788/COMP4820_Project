26d6272d429195ce025eaff47207fbee81b05146
SahilMahey <smahey@mun.ca>
2023-11-02 18:23:50 -0230

    changes  in _login_singup_frontend

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -12,6 +12,7 @@ function App() {
       
         <Route index element={<Login />}/>
         <Route path ="Signup" element = {<Signup/>}></Route>
+        <Route path="login" element={<Login/>} > </Route>
     </Routes>
     </BrowserRouter>
   );
