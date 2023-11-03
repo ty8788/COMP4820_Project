5120409da510545e4fa8f8413ead2954d5f8c4fa
Tyler Morgan <113244363+ty8788@users.noreply.github.com>
2023-11-03 15:13:32 -0230

    Add files via upload

---
+++ frontend/my_app/src/pages/signup.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import './signup.css'; 
+import { Link } from 'react-router-dom';
+
+
+export default function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  
+  
+
+  
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // Handling the form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      alert('enter all values')
+      setSubmitted(false);
+    } else {
+      setSubmitted(true);
+      
+    }
+  };
+
+
+  return (
+    <div className="form">
+      <div>
+        <h1>User Registration</h1>
+      </div>
+      <form>
+        <label className="label_name">Name</label>
+        <input onChange={handleName} className="input_name" value={name} type="text" />
+
+        <label className="label_email">Email</label>
+        <input onChange={handleEmail} className="input_email" value={email} type="email" />
+
+        <label className="label_username">Username</label>
+        <input onChange={handleUsername} className="input_username" value={username} type="username" />
+
+        <label className="label_password">Password</label>
+        <input onChange={handlePassword} className="input_password" value={password} type="password" />
+
+        <button onClick={handleSubmit} className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+      {
+      submitted && <div className="user_added">
+        <p>User registered Sucessfully</p>
+        
+        </div>
+      }
+    </div>
+  );
+}
